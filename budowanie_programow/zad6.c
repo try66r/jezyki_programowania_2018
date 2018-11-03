b612a588f7944bdba7376fe6665407c8cb71644e
try66r <pbranewski@sigma.ug.edu.pl>
2018-11-03 11:56:07 +0100

    Add zad6.c

---
+++ budowanie_programow/zad6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+void wczytaj_dane(double* x);
+void wypisz_wynik(double x);
+
+int main(){
+  double sr;
+  wczytaj_dane(&sr);
+  wypisz_wynik(sr);
+  return 0;
+}
+
+void wczytaj_dane(double* x){
+  printf("Program oblicza pierwiastek kwadratowy z liczby rzeczywistej.\n");
+  printf("Podaj x: ");
+  scanf("%lf", x);
+}
+
+void wypisz_wynik(double x){
+  if(x >= 0){
+    printf("Pierwiastek z liczby x = %lf wynosi %lf\n", x, sqrt(x));
+  }
+  else{
+    printf("Liczba ujemna!\n");
+  }
+}
