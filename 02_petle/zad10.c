0bb69f0760c35f1becd07bb1e959ed2104aa31a5
try66r <pbranewski@sigma.ug.edu.pl>
2018-11-20 15:16:28 +0100

    Add zad10.c

---
+++ 02_petle/zad10.c
@@ -0,0 +1,31 @@
+// Napisać program obliczający silnię z podanej,
+// niezbyt dużej, liczby naturalnej.
+
+#include<stdio.h>
+
+void take_input(int *user_input);
+void count_power_and_print_out(int user_input);
+
+int main()
+{
+  int user_input;
+  take_input(&user_input);
+  count_power_and_print_out(user_input);
+}
+
+void take_input(int *user_input)
+{
+  printf("Podaj niezbyt duza liczbe naturalna: ");
+  scanf("%d", user_input);
+}
+
+void count_power_and_print_out(int user_input)
+{
+  int res = 1;
+  while(user_input > 0)
+  {
+    res = res * user_input;
+    user_input--;
+  }
+  printf("Silnia wynosi: %d\n", res);
+}
